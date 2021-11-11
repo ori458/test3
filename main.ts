9e383b6f86c3949308e756dba8c6db1b18dd9523
ori458 <94099307+ori458@users.noreply.github.com>
2021-11-11 16:46:55 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,7 @@
+kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 0)
+basic.forever(function () {
+    THK.モーター番号_角度(0, 5)
+})
+basic.forever(function () {
+	
+})
---
+++ thk.ts
@@ -0,0 +1,26 @@
+/**
+* このファイルを使って、独自の関数やブロックを定義してください。
+* 詳しくはこちらを参照してください：https://makecode.microbit.org/blocks/custom
+*/
+
+
+/**
+ * Custom blocks
+ */
+//% weight=150 color=#0fbc11 icon=""
+namespace THK {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block
+    export function モーター番号_角度(e: number, n: number): void {
+        // Add code here
+        n = n * 0.9;
+        kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, n)
+        //kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, n)
+    }
+
+}// ここにコードを追加します
