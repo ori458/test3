81a179d0a67b8859f27d7f18cc34e6dcec5f068a
ori458 <94099307+ori458@users.noreply.github.com>
2021-11-11 16:59:56 +0900

    更新 thk.ts

--- thk.ts
+++ thk.ts
@@ -1,25 +1,159 @@
 /**
-* このファイルを使って、独自の関数やブロックを定義してください。
-* 詳しくはこちらを参照してください：https://makecode.microbit.org/blocks/custom
-*/
-
-
-/**
- * Custom blocks
+ * Blocks for driving the Kitronik I2C 16-Servo Driver Board
  */
-//% weight=150 color=#0fbc11 icon=""
+//% weight=100 color=#00A654 icon="\uf085" block="I2C 16-Servo"
+
 namespace THK {
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
-    //% block
-    export function モーター１の角度(n: number): void {
-        // Add code here
-        n = n * 0.8;
-        kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, n)
+
+    //Some useful parameters. 
+    let ChipAddress = 0x6A //default Kitronik Chip address
+    let PrescaleReg = 0xFE //the prescale register address
+    let Mode1Reg = 0x00  //The mode 1 register address
+
+    // If you wanted to write some code that stepped through the servos then this is the BASe and size to do that 	
+    let Servo1RegBase = 0x08
+    let ServoRegDistance = 4
+    //To get the PWM pulses to the correct size and zero offset these are the default numbers. 
+    let ServoMultiplier = 226
+    let ServoZeroOffset = 0x66
+
+    let initalised = false //a flag to allow us to initialise without explicitly calling the secret incantation
+
+    //nice big list of servos for the block to use. These represent register offsets in the PCA9865
+    export enum Servos {
+        Servo1 = 0x08,
+        Servo2 = 0x0C,
+        Servo3 = 0x10,
+        Servo4 = 0x14,
+        Servo5 = 0x18,
+        Servo6 = 0x1C,
+        Servo7 = 0x20,
+        Servo8 = 0x24,
+        Servo9 = 0x28,
+        Servo10 = 0x2C,
+        Servo11 = 0x30,
+        Servo12 = 0x34,
+        Servo13 = 0x38,
+        Servo14 = 0x3C,
+        Servo15 = 0x40,
+        Servo16 = 0x44,
     }
 
-}// ここにコードを追加します
+    export enum BoardAddresses {
+        Board1 = 0x6A,
+
+    }
+    //Trim the servo pulses. These are here for advanced users, and not exposed to blocks.
+    //It appears that servos I've tested are actually expecting 0.5 - 2.5mS pulses, 
+    //not the widely reported 1-2mS 
+    //that equates to multiplier of 226, and offset of 0x66
+    // a better trim function that does the maths for the end user could be exposed, the basics are here 
+    // for reference
+
+    export function TrimServoMultiplier(Value: number) {
+        if (Value < 113) {
+            ServoMultiplier = 113
+        }
+        else {
+            if (Value > 226) {
+                ServoMultiplier = 226
+            }
+            else {
+                ServoMultiplier = Value
+            }
+
+        }
+    }
+    export function TrimServoZeroOffset(Value: number) {
+        if (Value < 0x66) {
+            ServoZeroOffset = 0x66
+        }
+        else {
+            if (Value > 0xCC) {
+                ServoZeroOffset = 0xCC
+            }
+            else {
+                ServoZeroOffset = Value
+            }
+
+        }
+    }
+
+    /*
+        This secret incantation sets up the PCA9865 I2C driver chip to be running at 50Hx pulse repetition, and then sets the 16 output registers to 1.5mS - centre travel.
+        It should not need to be called directly be a user - the first servo write will call it.
+	
+    */
+    function secretIncantation(): void {
+        let buf = pins.createBuffer(2)
+
+        //Should probably do a soft reset of the I2C chip here when I figure out how
+
+        // First set the prescaler to 50 hz
+        buf[0] = PrescaleReg
+        buf[1] = 0x85
+        pins.i2cWriteBuffer(ChipAddress, buf, false)
+        //Block write via the all leds register to set all of them to 90 degrees
+        buf[0] = 0xFA
+        buf[1] = 0x00
+        pins.i2cWriteBuffer(ChipAddress, buf, false)
+        buf[0] = 0xFB
+        buf[1] = 0x00
+        pins.i2cWriteBuffer(ChipAddress, buf, false)
+        buf[0] = 0xFC
+        buf[1] = 0x66
+        pins.i2cWriteBuffer(ChipAddress, buf, false)
+        buf[0] = 0xFD
+        buf[1] = 0x00
+        pins.i2cWriteBuffer(ChipAddress, buf, false)
+        //Set the mode 1 register to come out of sleep
+        buf[0] = Mode1Reg
+        buf[1] = 0x01
+        pins.i2cWriteBuffer(ChipAddress, buf, false)
+        //set the initalised flag so we dont come in here again automatically
+        initalised = true
+    }
+
+
+    /**
+         * sets the requested servo to the reguested angle.
+         * if the PCA has not yet been initialised calls the initialisation routine
+         *
+         * @param Servo Which servo to set
+         * @param degrees the angle to set the servo to
+         */
+    //% blockId=kitronik_I2Cservo_write
+    //% block="set%Servo|to%degrees"
+    //% degrees.min=0 degrees.max=180
+
+    export function サーボモータ(Servo: Servos, degrees: number): void {
+        if (initalised == false) {
+            secretIncantation()
+        }
+        let buf = pins.createBuffer(2)
+        let HighByte = false
+        let deg100 = degrees * 50 //元は100
+        let PWMVal100 = deg100 * ServoMultiplier
+        let PWMVal = PWMVal100 / 10000
+        PWMVal = Math.floor(PWMVal)
+        PWMVal = PWMVal + ServoZeroOffset
+        if (PWMVal > 0xFF) {
+            HighByte = true
+        }
+        buf[0] = Servo
+        buf[1] = PWMVal
+        pins.i2cWriteBuffer(ChipAddress, buf, false)
+        if (HighByte) {
+            buf[0] = Servo + 1
+            buf[1] = 0x01
+        }
+        else {
+            buf[0] = Servo + 1
+            buf[1] = 0x00
+        }
+        pins.i2cWriteBuffer(ChipAddress, buf, false)
+    }
+
+
+}
+    
